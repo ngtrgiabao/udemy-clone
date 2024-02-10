c9e8e492816134438bc7b4c0d0782247af443de1
ngtrgiabao <ngtrgiabao251002@gmail.com>
2024-02-10 13:36:15 +0700

    feat: add price

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
-import { LayoutDashboard } from "lucide-react"
+import { CircleDollarSign, CircleDollarSignIcon, File, LayoutDashboard, ListChecks } from "lucide-react"
 
 import { db } from "@/lib/db"
 import { IconBadge } from "@/components/icon-badge"
@@ -8,6 +8,7 @@ import { TitleForm } from "./_components/title-form"
 import { DescriptionForm } from "./_components/description-form"
 import { ImageForm } from "./_components/image-form"
 import { CategoryForm } from "./_components/category-form"
+import { PriceForm } from "./_components/price-form"
 
 const CouseIdPage = async (
   { params }: {
@@ -91,6 +92,39 @@ const CouseIdPage = async (
               value: category.id
             }))}
           />
+        </div>
+        <div className="space-y-6">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={ListChecks} />
+              <h2 className="text-xl">
+                Course chapters
+              </h2>
+            </div>
+            <div>
+              TODO: Chapters
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={CircleDollarSignIcon} />
+              <h2 className="text-xl">
+                Sell your course
+              </h2>
+            </div>
+            <PriceForm 
+              initialData={course}
+              courseId={course.id}
+            />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">
+                Resources & Attachments
+              </h2>
+            </div>            
+          </div>
         </div>
       </div>
     </div>
