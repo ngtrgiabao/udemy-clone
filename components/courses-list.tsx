0cc4c7ad5b89e2405df7edff5c490d87d155fa23
ngtrgiabao <ngtrgiabao251002@gmail.com>
2024-02-15 17:02:29 +0700

    chores: typo

--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -35,7 +35,7 @@ export const CoursesList = ({
       {
         items.length === 0 && (
           <div className="text-center text-sm text-muted-foreground mt-10">
-            No coures found
+            No courses found
           </div>
         )
       }
