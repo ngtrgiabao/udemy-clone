5048eb9258fccefb74b9f11d1682da2ab90bb091
ngtrgiabao <ngtrgiabao251002@gmail.com>
2024-02-15 11:24:12 +0700

    feat: stripe config

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -4,12 +4,16 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { VideoPlayer } from "./_components/video-player";
+import { CourseEnrollButton } from "./_components/course-enroll-button";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
+import { File } from "lucide-react";
 
 const ChapterIdPage = async (
   { params }: {
     params: {
       courseId: string;
-      chapterId: string
+      chapterId: string;
     }
   }
 ) => {
@@ -60,7 +64,7 @@ const ChapterIdPage = async (
       }
       <div className="flex flex-col max-w-4xl mx-auto pb-20">
         <div className="p-4">
-          <VideoPlayer 
+          <VideoPlayer
             chapterId={params.chapterId}
             title={chapter.title}
             courseId={params.courseId}
@@ -70,6 +74,51 @@ const ChapterIdPage = async (
             completeOnEnd={completeOnEnd}
           />
         </div>
+        <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+          <h2 className="text-2xl font-semibold mb-2">
+            {chapter.title}
+          </h2>
+          {
+            purchase ? (
+              <div>
+                {/* TODO: add course progress button */}
+              </div>
+            ) : (
+              <CourseEnrollButton
+                courseId={params.courseId}
+                price={course.price!}
+              />
+            )
+          }
+        </div>
+        <Separator />
+        <div>
+          <Preview value={chapter.description!} />
+        </div>
+        {
+          !!attachments.length && (
+            <>
+              <Separator />
+              <div className="p-4">
+                {
+                  attachments.map(attachment => (
+                    <a
+                      key={attachment.id}
+                      href={attachment.url}
+                      target="_blank"
+                      className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                    >
+                      <File />
+                      <p className="line-clamp-1">
+                        {attachment.name}
+                      </p>
+                    </a>
+                  ))
+                }
+              </div>
+            </>
+          )
+        }
       </div>
     </div>
   );
